2af2ed0b7886920840976c3aec27f3c4f4e8a095
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2025-10-08 16:19:35 -0300

    Update ex03.js

--- FuncoesJs/src/ex03.js
+++ FuncoesJs/src/ex03.js
@@ -3,8 +3,5 @@
 let desconto = (preço, desconto) => {
     return preço - (preço * desconto) / 100
 }
+
 console.log(desconto(500, 15))
-
-let divisão = (n1, n2) => n1 / n2
-
-console.log(divisão(20, 2))
